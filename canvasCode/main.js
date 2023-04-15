acd040accededdadda3f131dc69c9e50bb34966b
Yuri Tsukahara <yuritsukahara@outlook.com>
2023-04-15 03:41:18 -0300

    trigger test

--- canvasCode/main.js
+++ canvasCode/main.js
@@ -57,19 +57,19 @@ function draw() {
 	singularity.show();
 }
 
-// function mousePressed() {
-// 	particles.push(
-// 		new Particle(
-// 			mouseX,
-// 			mouseY,
-// 			...[, ,],
-// 			random(0, 255),
-// 			random(0, 255),
-// 			random(0, 255),
-// 			frameCount
-// 		)
-// 	);
-// }
+function mousePressed() {
+	particles.push(
+		new Particle(
+			mouseX,
+			mouseY,
+			...[, ,],
+			random(0, 255),
+			random(0, 255),
+			random(0, 255),
+			frameCount
+		)
+	);
+}
 
 function keyPressed() {
 	console.log(keyboardMap[keyCode]);
@@ -91,7 +91,7 @@ function keyPressed() {
 
 function touchStarted() {
 	hiddenInput.elt.focus();
-	prompt();
+	$(hiddenInput).trigger('focus');
 }
 
 function playSynth() {
